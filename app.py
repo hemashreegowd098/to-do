ead187eb0aa6e9f64ce8184d1c1d56418f40b058
hemashreegowd098 <hemashreegowd098@gmail.com>
2026-04-05 12:40:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__,
-            template_folder='to-do-list/frontend/templates',
-            static_folder='to-do-list/frontend/static')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
